80ced8b76503f5a462daa84411127580ed4f7cd2
kblack-git <keithblack@gmail.com>
2023-05-23 15:41:43 -0400

    refactored CIK to 10 digits

--- src/components/StockLookup.js
+++ src/components/StockLookup.js
@@ -1,107 +1,207 @@
+// import { useState, useEffect } from "react";
+// import StockDetails from "./StockDetails";
+// import { type } from "@testing-library/user-event/dist/type";
+
+
+// const StockLookup = (props) => {
+//     var symbol = ''
+//     const [textBoxValue, setTextBoxValue] = useState("")
+//     const [stockSymbol, setStockSymbol] = useState("")
+//     const [cikString, setCikString] = useState("")
+
+
+//     // const [stock, setStock]= useState([])
+//     const [companyName, setCompanyName] = useState([])
+
+//     const normalizeCik = (cik) => {
+//         console.log(cik.length)
+
+
+//     }
+
+
+//   async function getJSON(){
+
+//     const response = await fetch("company_tickers.json")
+//     const data = await response.json()    
+//     for (let i = 0; i < Object.keys(data).length; i++) {
+//         if (data[i].ticker == `${stockSymbol}`) {
+//             setCompanyName(data[i].title)
+//             //   setCikString(data[i].cik_str)
+//             setCikString(data[i].cik_str.toString().padStart(10, '0'))
+            
+            
+//         }
+//     }
+// }
+
+
+
+//     async function fetchData(){
+
+//         console.log('cikString=' + cikString)
+//         await getJSON()
+//         const response = await fetch(`https://data.sec.gov/submissions/CIK${cikString}.json`)
+//         const data = await response.json()
+//         console.log(data)
+//     }
+
+  
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//         setStockSymbol(textBoxValue)
+//         fetchData()
+//         setTextBoxValue("")
+
+//     }
+//     return (
+//         <>
+
+//             <form onSubmit={handleSubmit}>
+//                 <label>Enter the stock symbol or Company Name</label>
+//                 <input
+//                     type="text"
+//                     value={textBoxValue}
+//                     onChange={e => setTextBoxValue(e.target.value)}
+//                 />
+//                 <button>Submit</button>
+//             </form>
+//             <StockDetails ticker={stockSymbol} name={companyName} cik={cikString} />
+
+//         </>
+//     )
+// }
+
+// export default StockLookup
+
+// import { useState } from "react";
+// import StockDetails from "./StockDetails";
+// const StockLookup = () => {
+//   const [textBoxValue, setTextBoxValue] = useState("");
+//   const [stockSymbol, setStockSymbol] = useState("");
+//   const [cikString, setCikString] = useState("");
+//   const [companyName, setCompanyName] = useState("");
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     setStockSymbol(textBoxValue);
+//     try {
+//       const response = await fetch("company_tickers.json");
+//       const data = await response.json();
+//       for (let i = 0; i < Object.keys(data).length; i++) {
+//         if (data[i].ticker === textBoxValue) {
+//           setCompanyName(data[i].title);
+//           setCikString(data[i].cik_str.toString().padStart(10, '0'));
+//           console.log('test',cikString)
+//           break;
+//         }
+        
+  
+//     }
+//       const secResponse = await fetch(
+//         `https://data.sec.gov/submissions/CIK${cikString}.json`
+//       );
+//       const secData = await secResponse.json();
+//       console.log(secData);
+//     } catch (error) {
+//       // Handle any errors
+//       console.error(error);
+//     }
+//     setTextBoxValue("");
+//   };
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <label>Enter the stock symbol or Company Name</label>
+//         <input
+//           type="text"
+//           value={textBoxValue}
+//           onChange={(e) => setTextBoxValue(e.target.value)}
+//         />
+//         <button>Submit</button>
+//       </form>
+//       <StockDetails ticker={stockSymbol} name={companyName} cik={cikString} />
+//     </>
+//   );
+// };
+// export default StockLookup;
+
 import { useState, useEffect } from "react";
 import StockDetails from "./StockDetails";
-import { type } from "@testing-library/user-event/dist/type";
+const StockLookup = () => {
+  const [textBoxValue, setTextBoxValue] = useState("");
+  const [stockSymbol, setStockSymbol] = useState("");
+  const [cikString, setCikString] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [dataFromSecGov, setDataFromSecGov]= useState(null)
 
-
-const StockLookup = (props) => {
-    var symbol  =''
-    const [textBoxValue, setTextBoxValue] = useState("")         
-    const [stockSymbol, setStockSymbol] = useState("")
-    const [cikString, setCikString] = useState("")
-    
-    
-    // const [stock, setStock]= useState([])
-    const [companyName, setCompanyName] = useState([])
-
-    const normalizeCik = (cik) => {
-        console.log(cik.length)
-
-
-    }
-
-    fetch("company_tickers.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data)
-    // const dataArray = Array.from(data)
-    // dataArray.forEach(stock => {
-    //     if (stock.ticker===stockSymbol) {
-    //         setCompanyName(stock.title)
-    //         console.log(stock.title)
-    //     }else{
-    //         console.log('not found')
-    //     }
-    // })
-
-   
-    for (let i=0;i<Object.keys(data).length;i++) {
-      if (data[i].ticker==`${stockSymbol}`) {
-    
-          setCompanyName(data[i].title)
-
-          setCikString(data[i].cik_str)
-          console.log(cikString)
-
-           let fullCikStr = cikString.toString().padStart(10,'0')
-        console.log(fullCikStr.length)
-        console.log(typeof(fullCikStr))
-        console.log(fullCikStr)
-        let fullcikint = fullCikStr + 0
-        console.log(fullcikint)
-        console.log(typeof(fullcikint))
-        // setCikString(Number(fullCikStr))
-           //   setCikString(cikString.toString().padStart(10,'0'))
-        //   setCikString(Number(fullCikStr))
-        
-       
-    
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("company_tickers.json");
+        const data = await response.json();
+        for (let i = 0; i < Object.keys(data).length; i++) {
+          if (data[i].ticker === textBoxValue) {
+            setCompanyName(data[i].title);
+            const cik = data[i].cik_str.toString().padStart(10, "0");
+            setCikString(cik);
+            break;
+          }
+        }
+      } catch (error) {
+        console.error(error);
       }
-}
-   })
-         
-const fetchData = () =>{
-    fetch("https://data.sec.gov/submissions/CIK0000320193.json")
-    .then((response) => response.json())
-    .then((data) => (data.name));
+    };
+    if (textBoxValue !== "") {
+      fetchData();
     }
-  
-    useEffect(() => {
-      fetchData()
-      // fetchCompany()
-    },[])
-     
+  }, [textBoxValue]);
+
+  useEffect(() => {
+    const fetchSecData = async () => {
+      if (cikString !== "") {
+        try {
+          const response = await fetch(
+            `https://data.sec.gov/submissions/CIK${cikString}.json`
+          );
+          const data = await response.json();
+          console.log('secGovFetch', data)
+          setDataFromSecGov(data);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    fetchSecData();
+  }, [cikString]);
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setStockSymbol(textBoxValue);
+    setTextBoxValue("");
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Enter the stock symbol or Company Name</label>
+        <input
+          type="text"
+          value={textBoxValue}
+          onChange={(e) => setTextBoxValue(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+      <StockDetails ticker={stockSymbol} name={companyName} cik={cikString} />
+    </>
+  );
+};
+export default StockLookup;
 
 
 
 
 
-
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        setStockSymbol(textBoxValue)
-        setTextBoxValue("")
-        
-    }
-    return (
-        <>
-        
-            <form onSubmit={handleSubmit}>
-                <label>Enter the stock symbol or Company Name</label>
-                <input 
-                    type="text" 
-                    value={textBoxValue}
-                    onChange={e => setTextBoxValue(e.target.value)}
-                />
-            <button>Submit</button>
-            </form>
-            <StockDetails ticker={stockSymbol} name={companyName} cik={cikString}/>
-            
-        </>
-    )
-}
-
-export default StockLookup
 
 
